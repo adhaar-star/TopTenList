81f892924182071d260c0a99f17819b446bef8d9
adhaar-star <59237202+adhaar-star@users.noreply.github.com>
2020-04-01 16:12:13 -0700

    Implementing display_forward and display_backward functions in TopTenList.cpp file

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,7 +1,9 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
+ using namespace std;
 
 using std::string;
 
@@ -21,15 +23,28 @@ Hyperlink TopTenList::get(int index)
 return _list[index-1];
 }
 
-
+// Display Top Ten Languages from beginning to end
 void TopTenList::display_forward(){
-    for(vector<Hyperlink>::iterator it = _list.begin();it!=_list.end();++it){
-        printf("%s\n",it->text.c_str());
+  //  for(vector<Hyperlink>::iterator it = _list.begin();it!=_list.end();++it){
+        for(int i=0; i<_list.size(); i++)
+  {
+         cout<<i+1<<" of "<<_list.size()<<" "
+             <<_list[i].text<<" "
+             <<_list[i].url<<std::endl;
+       // printf("%s\n",it->text.c_str());
     }
+//}
 }
 
+// Display Top Ten Languages from end to begin
 void TopTenList::display_backward(){
-    for(vector<Hyperlink>::reverse_iterator it = _list.rbegin();it!=_list.rend();++it){
-        printf("%s\n",it->text.c_str());
+   // for(vector<Hyperlink>::reverse_iterator it = _list.rbegin();it!=_list.rend();++it){
+           for(int i=_list.size()-1; i>0; i--)
+  {
+         cout<<i+1<<" of "<<_list.size()<<" "
+             <<_list[i].text<<" "
+             <<_list[i].url<<std::endl;
+       // printf("%s\n",it->text.c_str());
     }
+    //}
 }
