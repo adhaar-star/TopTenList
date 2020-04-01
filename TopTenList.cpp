9ca087474e55b08446717ec7e0319e6e2845ef0b
adhaar-star <59237202+adhaar-star@users.noreply.github.com>
2020-04-01 15:55:36 -0700

    Implementing display_forward and display_backward functions in TopTenList.cpp file

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,15 +7,29 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-
-}
+    
+_list.resize(10);
+};
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+_list[index-1] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
+return _list[index-1];
+}
 
+
+void TopTenList::display_forward(){
+    for(vector<Hyperlink>::iterator it = _list.begin();it!=_list.end();++it){
+        printf("%s\n",it->text.c_str());
+    }
+}
+
+void TopTenList::display_backward(){
+    for(vector<Hyperlink>::reverse_iterator it = _list.rbegin();it!=_list.rend();++it){
+        printf("%s\n",it->text.c_str());
+    }
 }
